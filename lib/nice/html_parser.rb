33e8ea99c4c1849a5f8e9675f5fffadc2bf59124
Benjamin Müller <elchbenny@googlemail.com>
2012-05-22 16:36:40 +0200

    basic support of multiple states including the same element

--- lib/nice/html_parser.rb
+++ lib/nice/html_parser.rb
@@ -8,7 +8,7 @@ module Nice
     def self.remove_elements_not_of_state(state, doc)
 
       doc.css("[data-state]").each do |node|
-        if node.attribute('data-state').value != state then
+        if !node.attribute('data-state').value.split(" ").include?(state) then
           node.remove
         end
       end
@@ -19,11 +19,12 @@ module Nice
   	def self.add_elements_of_current_state doc, curr_state
 
   		# get all nodes of the current state
-  		curr_state_nodes = doc.css("[data-state='#{curr_state}']")
+  		curr_state_nodes = doc.css("[data-state~='#{curr_state}']")
 
   		# get reference nodes in DOM tree for current nodes and generate js insert statements
   		stack = curr_state_nodes.reverse.each_with_index.map do |curr_node,index|
-  			ref_id = self.ref_node_uid(curr_node['data-state'],curr_state_nodes.count - index)
+
+  			ref_id = self.ref_node_uid(curr_state,curr_state_nodes.count - index)
   			ref_node_name = "[data-state-uid~=\'#{ref_id}\']"  			
   			ref_node = doc.css(ref_node_name)
 
@@ -55,49 +56,55 @@ module Nice
   		per_state_counter = {}
   		doc.css("[data-state]").each do |curr_node|
   			
-  			# increase counter per state
-  			per_state_counter[curr_node.attribute("data-state").value] ||= 0
-  			idx = per_state_counter[curr_node.attribute("data-state").value] += 1
+  			# each node can have multiple state references which need to be
+  			# treated separately
+  			states = curr_node.attribute("data-state").value.split(" ")
 
-  			# try using preceding element if one exists otherwise use parent.
-  			# the referencing node must not be an state bound element otherwise
-  			# we can not be sure the element is always present.
-  			prev_node = curr_node.previous_element
+  			states.each do |state|
+	  			# increase counter per state
+	  			per_state_counter[state] ||= 0
+	  			idx = per_state_counter[state] += 1
 
-  			while prev_node && prev_node.has_attribute?("data-state")
-  			 	prev_node = prev_node.previous_element
-  			end 
+	  			# try using preceding element if one exists otherwise use parent.
+	  			# the referencing node must not be an state bound element otherwise
+	  			# we can not be sure the element is always present.
+	  			prev_node = curr_node.previous_element
 
-  			if prev_node && !prev_node.has_attribute?("data-state")
-  				node = prev_node
-  				method = "insert"	
-  			else
-  				par_node = curr_node.parent
-  				
-  				while par_node && par_node.has_attribute?("data-state")
-  			 		par_node = par_node.parent
-  				end 
+	  			while prev_node && prev_node.has_attribute?("data-state")
+	  			 	prev_node = prev_node.previous_element
+	  			end 
 
-  				if par_node && !par_node.has_attribute?("data-state")
-  					node = curr_node.parent
-  					method = "append"
-  				else
-  					raise "No reference could be created for node #{curr_node}. Make sure this node \
-  					is preceeded or sourrounded at least by one non state bound element."
-  				end
-  				
-  			end
+	  			if prev_node && !prev_node.has_attribute?("data-state")
+	  				node = prev_node
+	  				method = "insert"	
+	  			else
+	  				par_node = curr_node.parent
+	  				
+	  				while par_node && par_node.has_attribute?("data-state")
+	  			 		par_node = par_node.parent
+	  				end 
 
-  			continue if node == nil
+	  				if par_node && !par_node.has_attribute?("data-state")
+	  					node = curr_node.parent
+	  					method = "append"
+	  				else
+	  					raise "No reference could be created for node #{curr_node}. Make sure this node \
+	  					is preceeded or sourrounded at least by one non state bound element."
+	  				end
+	  				
+	  			end
 
-  			# add reference to the found element
-  			a = node.has_attribute?('data-state-uid') ? [node.attribute('data-state-uid').value] : []
-			a += [self.ref_node_uid(curr_node['data-state'],idx)]
-			node['data-state-uid'] = a.join(" ")
+	  			continue if node == nil
 
-			m = node.has_attribute?('data-state-insert-method') ? [node.attribute('data-state-insert-method').value] : []
-			m += [method]
-			node['data-state-insert-method'] = m.join(" ")
+	  			# add reference to the found element
+	  			a = node.has_attribute?('data-state-uid') ? [node.attribute('data-state-uid').value] : []
+				a += [self.ref_node_uid(state,idx)]
+				node['data-state-uid'] = a.join(" ")
+
+				m = node.has_attribute?('data-state-insert-method') ? [node.attribute('data-state-insert-method').value] : []
+				m += [method]
+				node['data-state-insert-method'] = m.join(" ")
+			end
   		end
 
   		doc
